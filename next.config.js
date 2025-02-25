26174861aae8c80b2391d5fe8c4b17f367d715bb
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2025-02-24 20:44:07 -0500

    redirects

---
+++ next.config.js
@@ -0,0 +1,43 @@
+const { createStylexPlugin } = require('@stylexjs/nextjs-plugin');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/mac',
+        destination: 'https://raw.githubusercontent.com/Jaredy899/mac/main/setup.sh',
+        permanent: true,
+      },
+      {
+        source: '/win',
+        destination: 'https://raw.githubusercontent.com/Jaredy899/win/main/first-setup.ps1',
+        permanent: true,
+      },
+      {
+        source: '/linux',
+        destination: 'https://raw.githubusercontent.com/Jaredy899/linux/refs/heads/main/linux.sh',
+        permanent: true,
+      },
+      {
+        source: '/linuxdev',
+        destination: 'https://raw.githubusercontent.com/Jaredy899/linux/refs/heads/dev/linux.sh',
+        permanent: true,
+      },
+      {
+        source: '/debian',
+        destination: 'https://raw.githubusercontent.com/Jaredy899/linux/refs/heads/main/config_changes/preseed.cfg',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+const stylexPlugin = createStylexPlugin({
+  rootDir: __dirname,
+  aliases: {
+    '@/*': ['./*'],
+  },
+});
+
+module.exports = stylexPlugin(nextConfig); 
